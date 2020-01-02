f6a91c0935793eb03255e4bbb33a821ef08af109
lokpro <cellurashin@gmail.com>
2020-01-02 17:13:32 +0800

    v1.7 fixed: works in "EDIT"

--- OPRcommentTextInput.user.js
+++ OPRcommentTextInput.user.js
@@ -1,7 +1,7 @@
-﻿// ==UserScript==
+// ==UserScript==
 // @name         OPRcommentTextInput
 // @description    更新前請記得備分你的文字! Be sure to backup your "texts" setting be updating!
-// @version      1.6
+// @version      1.7
 // @namespace    sdfgsdfgerverververververv
 // @updateURL      https://github.com/Ingrass/OPR_CannedCommentTexts/raw/master/OPRcommentTextInput.user.js
 // @downloadURL    https://github.com/Ingrass/OPR_CannedCommentTexts/raw/master/OPRcommentTextInput.user.js
@@ -11,7 +11,15 @@
 // @grant        none
 // ==/UserScript==
 
+/*************************************
+*** 更新前請記得備分你的文字!
+*** Be sure to backup your "texts" setting be updating!
+*************************************/
+
 /*
+v1.7 2/Jan/2020
+- fixed: works in "EDIT"
+
 v1.6 11/10/2019
 - fixes for new WayFarer site
 */
@@ -32,15 +40,18 @@ for( var i=0; i<texts.length; i++ ){
 	html_textButtons += "<button class='button cannedCommentsButton' style='font-size: 12px;padding: 5px;'>"+texts[i]+"</button>";
 }
 
-var textBoxes = document.querySelectorAll("#additional-comments-card > .card__body");
-textBoxes[0].insertAdjacentHTML("beforebegin", "<div class='center' style='text-align: center'>" + html_textButtons + "</div>");
+var textBoxes = document.querySelectorAll(".comments-card > .card__body");
 
 var buttons = document.getElementsByClassName("cannedCommentsButton");
+
+textBoxes[0].insertAdjacentHTML("beforebegin", "<div class='center' style='text-align: center'>" + html_textButtons + "</div>");
+textBoxes[1].insertAdjacentHTML("beforebegin", "<div class='center' style='text-align: center'>" + html_textButtons + "</div>");
+
 for (var i=0; i<buttons.length; i++ ) {
+
 	buttons[i].addEventListener("click", function (event) {
 		var source = event.target || event.srcElement;
 
 		textBoxes[0].value = textBoxes[1].value = angular.element(document.getElementById('AnswersController')).scope().answerCtrl.formData.comment += source.innerText;
 	} );
 }
-
